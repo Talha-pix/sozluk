a018736db26143c85f0157c472f04b096ae616b7
Talha-pix <cml.ylmz6158@gmail.com>
2024-10-19 20:41:44 +0300

    Create sozluk.py

---
+++ sozluk.py
@@ -0,0 +1,8 @@
+meme_dict = {'MONEY':'para','CRINGE':'Garip ya da utandırıcı bir şey','LOL':'komik şeylere verilen cevap'}
+
+word = input('Anlamadığınız bir kelime yazın (tüm harfler büyük!!!):')
+
+ if word in meme_dict():
+     print(meme_dict[word]))
+ else:
+     print('!!!!BU KELİME SÖZLÜKTE YOK!!!!')
